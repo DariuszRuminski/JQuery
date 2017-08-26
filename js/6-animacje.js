df62e1ad265e116c26538e28bf07435f0a962f8f
DariuszRuminski <30296034+DariuszRuminski@users.noreply.github.com>
2017-08-26 14:23:41 +0200

    commit - animacje

---
+++ js/6-animacje.js
@@ -0,0 +1,28 @@
+$(function(){
+    
+    $("h2").click(function(){
+       $(this).hide(3000); 
+    });
+    
+    //Efekt hide i show
+    $("p").hide(3000).show(3000);
+    
+    //Efekt fadeOut i fadeIn
+    $("h1").fadeOut(3000).fadeIn(3000);  
+    
+    //Efekt slideUp i slideDown
+    $("h2").slideUp(3000).slideDown(3000);
+    
+    function animacja(){
+        console.log("Zakończono pierwszą animaczję. Zaraz startuje druga");
+        $("h3").animate({
+        "font-size": "1em",
+        "margin-left": "0px"
+    }, 3000);
+    }
+    
+    $("h3").animate({
+       "font-size": "3em",
+        "margin-left": "100px"
+    }, 2000, animacja);
+});
