ca82f95d267e837078032cc79ec1d7685ad602a4
DariuszRuminski <30296034+DariuszRuminski@users.noreply.github.com>
2017-08-26 12:34:36 +0200

    commit

---
+++ js/2-selektory.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(function(){
+   //pobieranie elementów po znaczniku
+    var paragraphs = $("p");
+    console.log(paragraphs);
+    
+    //pobieranie elementow po id
+    var paragraphOne = $("#parafraf");
+    console.log(paragraphOne);
+    
+    //pobieranie elementow po klasie
+    var paragraphTwo = $(".paragraf");
+    console.log(paragraphTwo);
+});
